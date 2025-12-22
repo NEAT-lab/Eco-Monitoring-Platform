dc2d23dfc714b8125ccbb33180a96664fdfacb6f
Ronoooie <sh10410807@gmail.com>
2025-12-22 17:48:35 +0800

    改善偵測功能與處理一些debug
    - 影片檔名加上時間，避免瀏覽器快取的原因取到舊的影片
    - 改善模型設定功能: 避免改到 sever 的全域 model 變數
    - 避免多使用者使用相同全域變數 total_count 與 count_by_class ，改善偵測數量為區域變數

--- static/panorama.js
+++ static/panorama.js
@@ -1,3 +1,28 @@
+let modelPath = "";
+
+async function setModel() {
+    modelPath = document.getElementById("modelSelect").value.trim();
+    if (!modelPath) {
+        alert("請輸入模型路徑");
+        return;
+    }
+    alert(`已選擇模型: ${modelPath}`);
+}
+
+window.onload = async function () {
+    const res = await fetch("/get_models");
+    const data = await res.json();
+    const select = document.getElementById("modelSelect");
+    data.models.forEach(m => {
+        const opt = document.createElement("option");
+        opt.value = `pt/${m}`;  // 完整路徑
+        opt.innerText = m;
+        select.appendChild(opt);
+    });
+    modelPath = "pt/yolov8n.pt";  // 預設模型
+    select.value = modelPath;
+}
+
 async function upload() {
     const file = document.getElementById("file").files[0];
     if (!file) {
@@ -7,6 +32,7 @@ async function upload() {
 
     let form = new FormData();
     form.append("file", file);
+    form.append("model_path", modelPath);
 
     document.getElementById("wait").innerText = "處理中，請稍候...";
     document.getElementById("resultImg").style.display = "none";
